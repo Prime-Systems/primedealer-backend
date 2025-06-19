d81a7e2785d88a45b31afdded108f323e0b14bb6
Jonathan Ato Markin <jona10711@gmail.com>
2025-06-19 16:49:50 +0200

    User record defined

--- user-service/src/main/java/com/prime/userservice/domain/User.java
+++
@@ -1,4 +0,0 @@
-package com.prime.userservice.domain;
-
-public record User() {
-}
---
+++ user-service/src/main/java/com/prime/userservice/domain/models/UserRole.java
@@ -0,0 +1,10 @@
+package com.prime.userservice.domain.models;
+
+public enum UserRole {
+    BUYER,
+    SELLER,
+    ADMIN,
+    SYSTEM_ADMIN,
+    GUEST,
+    SHIPPER
+}
