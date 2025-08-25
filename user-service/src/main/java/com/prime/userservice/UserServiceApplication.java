ec56a994a59e6b39a8b62bd59976dfb064af29f2
Jonathan Ato Markin <jona10711@gmail.com>
2025-08-25 15:46:51 +0200

    fix

--- user-service/src/main/java/com/prime/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/prime/userservice/UserServiceApplication.java
@@ -1,7 +1,10 @@
 package com.prime.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -9,4 +12,16 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+}
+
+@Component
+class MyRunner implements CommandLineRunner {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Database URL: " + dbUrl);
+    }
 }
